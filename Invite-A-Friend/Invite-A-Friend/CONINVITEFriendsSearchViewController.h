b85117bfef4f6512e86b53845f9b5d79f39360d0
markconcept <mark@concept96.co.uk>
2014-02-02 14:10:41 +0000

    Added the new .h and .m files for the search function

---
+++ Invite-A-Friend/Invite-A-Friend/CONINVITEFriendsSearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  CONINVITEFriendsSearchViewController.h
+//  Invite-A-Friend
+//
+//  Created by Mark McWhirter on 02/02/2014.
+//  Copyright (c) 2014 Concept96. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CONINVITEFriendsSearchViewController : UIViewController
+
+@end
---
+++ Invite-A-Friend/Invite-A-Friend/CONINVITEFriendsSearchViewController.m
@@ -0,0 +1,38 @@
+//
+//  CONINVITEFriendsSearchViewController.m
+//  Invite-A-Friend
+//
+//  Created by Mark McWhirter on 02/02/2014.
+//  Copyright (c) 2014 Concept96. All rights reserved.
+//
+
+#import "CONINVITEFriendsSearchViewController.h"
+
+@interface CONINVITEFriendsSearchViewController ()
+
+@end
+
+@implementation CONINVITEFriendsSearchViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
