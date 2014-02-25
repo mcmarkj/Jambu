c75bb6084cf54ba3f0c7d2ae304f4796ed7afab5
markconcept <mark@concept96.co.uk>
2014-02-25 20:39:38 +0000

    You can now open up a view from a view from a view from a view

--- Invite-A-Friend/Invite-A-Friend/CONINVITEFriendsViewController.m
+++ Invite-A-Friend/Invite-A-Friend/CONINVITEFriendsViewController.m
@@ -268,7 +268,7 @@
  */
 
 
- // Override to support editing the table view.
+ /*// Override to support editing the table view.
  - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
  {
  if (editingStyle == UITableViewCellEditingStyleDelete) {
@@ -279,7 +279,7 @@
  // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
  }
  }
- 
+ */
 
 /*
  // Override to support rearranging the table view.
